4793b81dff84dd525c2c7270eed10554d1583fd5
Vahe Pezeshkian <vahepezeshkian@wlan-207160.nbw.tue.nl>
2019-01-31 12:04:54 +0100

    Decision tree regressor

---
+++ decision_tree.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 11:56:19 2019
+
+@author: vahepezeshkian
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('datasets/Position_Salaries.csv')
+x = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(x, y)
+
+y_pred = regressor.predict([[6.5]])
+
+### Visualizing
+plt.scatter(x, y, color = 'black')
+x_grid = np.arange(min(x), max(x), 0.1)
+x_grid = x_grid.reshape((len(x_grid), 1))
+plt.plot(x_grid, regressor.predict(x_grid), color = 'red')
+plt.title('Salary vs. Experience')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
